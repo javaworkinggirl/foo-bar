b58d094458a134330ba2538f5f17f46523a1ffd5
Tara Czutno <javaworkinggirl@aol.com>
2026-04-26 15:16:51 -0600

    Update tests

--- foo/src/test/java/com/example/foo/RedisWriterServiceIT.java
+++ foo/src/test/java/com/example/foo/RedisWriterServiceIT.java
@@ -24,7 +24,7 @@ class RedisWriterServiceIT {
     @DynamicPropertySource
     static void properties(DynamicPropertyRegistry registry) {
         TestPropertyRegistrar.registerRedis(registry, REDIS);
-        registry.add("app.s3.bucket-name", () -> "RedisWriterServiceIT-test-bucket");
+        registry.add("app.s3.bucket-name", () -> "test-bucket");
         registry.add("app.s3.region",      () -> "us-east-1");
     }
 
